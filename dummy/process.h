ae956bfe9c254ecbf7f63d85f579d72302cf1ddf
Yi-Shiuan Chen <y223chen@eceLinux2.uwaterloo.ca>
2011-03-06 14:36:00 -0500

    added the get_process_id function, also started implementing the forcibility running a process, but currently will not use it until the specification is clear

--- dummy/process.h
+++ dummy/process.h
@@ -13,6 +13,7 @@ void schedule_next_process_BACK();
 
 struct PCB* get_process_from_ID(int process_id);
 int get_process_ID();
+int get_process_priority_usagi_san(int process_id);
 int process_exists(int process_id);
 int is_waiting_for(int process_ID, int sender_ID);
 
@@ -20,5 +21,12 @@ int is_waiting_for(int process_ID, int sender_ID);
 // this funtion does not check the validity of the state, so make sure you know what 
 // you are doing with it.
 void set_process_state(int process_id, int process_state);
+
+// again, set the process with process ID to run.  Make sure to call scheduler
+// or release_processor aftercalling this function.  This will override
+// the default behavior of the scheduler and run the specified process instead
+// of the next most priority process.
+// Not used for now.
+void set_process_to_run(int process_id);
 
 #endif
