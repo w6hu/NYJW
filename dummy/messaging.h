b650aa1e5bf94bd297e228a47ca47e3b51652e18
Wan Hu <w6hu@eceLinux3.uwaterloo.ca>
2011-03-08 15:12:34 -0500

    purged branch

--- dummy/messaging.h
+++
@@ -1,9 +0,0 @@
-#include "rtx_inc.h"
-#include "memory.h"
-#include "process.h"
-#include "init.h"
-#include "rtx.h"
- 
-void init_mailboxes();
-int send_message_jessie (int process_ID, void * MessageEnvelope);
-void * receive_message_jessie (int * sender_ID);
